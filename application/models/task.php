054052c11acf8449a0322f714e720a0acc3f650a
Symeon Tsitoglou <simtsit@gmail.com>
2014-09-15 20:00:42 +0200

    Update model task.php
    Added function for loading a specific task based on task id.

--- application/models/task.php
+++ application/models/task.php
@@ -8,6 +8,12 @@ class Task extends CI_Model
    return $this->db->get('tasks')->result_array();
  }
 
+  public function get_task_by_id($taskid){
+    $this->db->where(array('active'=>1));
+    $this->db->where('id',$taskid);
+    return $this->db->get('tasks')->reulst_array();
+  }
+
   //public function get_tasks_by_project_id($project_id) {
   public function get_tasks_by_project_ids($projectids) {
     $this->db->where(array('active'=>1));
