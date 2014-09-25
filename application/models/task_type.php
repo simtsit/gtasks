ba1ab4a7e04c1aa16dd355da65a1b3f3d6fca1dd
Symeon Tsitoglou <simtsit@gmail.com>
2014-09-25 10:39:56 +0200

    Update task_type.php
    Added comments.

--- application/models/task_type.php
+++ application/models/task_type.php
@@ -1,11 +1,17 @@
 <?php
+
+/* This model is related to task types */
+
 class Task_type extends CI_Model
 {
 
+/* This function returns an array with all active task types */
  public function all_task_types() {
-   // $this->db->select('id, creator, owner, description');
    $this->db->where(array('active'=>1));
    return $this->db->get('task_types')->result_array();
  }
 
 }
+
+/* End of file task_type.php */
+/* Location: ./application/models/task_type.php */
