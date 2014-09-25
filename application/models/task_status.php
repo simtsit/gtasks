a93440c36e1a91f5a9af8278fd6314245b1efdef
Symeon Tsitoglou <simtsit@gmail.com>
2014-09-25 10:41:36 +0200

    Update task_status.php
    Added comments.

--- application/models/task_status.php
+++ application/models/task_status.php
@@ -1,11 +1,17 @@
 <?php
+
+/* This model is related to task status */
+
 class Task_Status extends CI_Model
 {
 
+/* This function return an array with all active task statuses */
  public function all_task_statuses() {
-   // $this->db->select('id, creator, owner, description');
    $this->db->where(array('active'=>1));
    return $this->db->get('task_statuses')->result_array();
  }
 
 }
+
+/* End of file task_status.php */
+/* Location: ./application/models/task_status.php */
