a39274c6c5d8d86db4ef378a620addf2ba3d02e5
Symeon Tsitoglou <simtsit@gmail.com>
2014-09-26 11:43:43 +0200

    Update task.php
    Added comments in the code.

--- application/models/task.php
+++ application/models/task.php
@@ -1,12 +1,20 @@
 <?php
+
+/* This model is related to Tasks. */
+
 class Task extends CI_Model
 {
 
+
+/* This function returns an array with all active tasks. */
+
  public function all_tasks() {
-   // $this->db->select('id, creator, owner, description');
    $this->db->where(array('active'=>1));
    return $this->db->get('tasks')->result_array();
  }
+
+
+/* This function returns an array with a single task based on task id. */
 
   public function get_task_by_id($taskid){
     $this->db->where(array('active'=>1));
@@ -14,12 +22,17 @@ class Task extends CI_Model
     return $this->db->get('tasks')->result_array();
   }
 
-  //public function get_tasks_by_project_id($project_id) {
+
+/* This function returns an array with all tasks that their project is in array $projectids. */
+
   public function get_tasks_by_project_ids($projectids) {
     $this->db->where(array('active'=>1));
     $this->db->where_in('project',$projectids);
     return $this->db->get('tasks')->result_array();
   }
+
+
+/* This function returns an array with all tasks of a project, based on project id. */
 
   public function get_tasks_by_project_id($projectid) {
     $this->db->where(array('active'=>1));
@@ -27,11 +40,17 @@ class Task extends CI_Model
     return $this->db->get('tasks')->result_array();
   }
 
+
+/* This function returns an array with all tasks set for a user, based on user id. */
+
   public function get_tasks_set_for($userid){
    $this->db->where(array('active'=>1));
    $this->db->where('setfor',$userid);
    return $this->db->get('tasks')->result_array();
   }
+
+
+/* This function returns an array with all tasks set by a user, based on user id. */
 
  public function get_tasks_set_from($userid){
   $this->db->where(array('active'=>1));
@@ -39,6 +58,9 @@ class Task extends CI_Model
   return $this->db->get('tasks')->result_array();
  }
 
+
+
+/* This function adds a task in the database. */
 
 public function insert_task($taskinfo) {
 
@@ -60,5 +82,7 @@ public function insert_task($taskinfo) {
      
     }
 
-
 }
+
+/* End of file task.php */
+/* Location: ./application/models/task.php */
