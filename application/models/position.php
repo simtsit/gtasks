282ac505875747e142035e24531e32e36e9e544c
Symeon Tsitoglou <simtsit@gmail.com>
2014-09-26 11:33:26 +0200

    Update position.php
    Added Comments

--- application/models/position.php
+++ application/models/position.php
@@ -1,11 +1,18 @@
 <?php
+
+/* This model is related to User Positions. */
+
 class Position extends CI_Model
 {
+
+/* This function returns an array with all active user positions. */
 
 	public function all_positions() {
 		$this->db->where(array('active'=>1));
 		return $this->db->get('positions')->result_array();
 	}
-
-
+	
 }
+
+/* End of file position.php */
+/* Location: ./application/models/position.php */
