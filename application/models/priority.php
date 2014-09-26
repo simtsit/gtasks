e63768c09625d8d3ace19be6408a73f57a77915c
Symeon Tsitoglou <simtsit@gmail.com>
2014-09-26 11:35:03 +0200

    Update priority.php
    Added Comments.

--- application/models/priority.php
+++ application/models/priority.php
@@ -1,10 +1,17 @@
 <?php
+
+/* This model is related to Task Priorities. */
+
 class Priority extends CI_Model
 {
+ 
+ /* This function returns an array with all task priorities. */
 
  public function all_priorities() {
-   // $this->db->where(array('active'=>1));
    return $this->db->get('priorities')->result_array();
  }
 
 }
+
+/* End of file priority.php */
+/* Location: ./application/models/priority.php */
